3731844371a4726826214e8b3f09b776e2970978
deanlauvina7 <deanlauvina7@gmail.com>
2020-10-19 09:52:40 -0500

    Coding  the js

---
+++ script.js
@@ -0,0 +1,31 @@
+// Selectors
+const countdown = document.querySelector('.countdown');
+const launchDate = new Date('Jan 1, 2021 13:00:00').getTime();
+
+// Update every second
+const intvl = setInterval(() => {
+  // Get today's date and time in (ms)
+  const now = new Date().getTime();
+  // Distance from now to the launch date
+  const distance = launchDate - now;
+  // Time calculations
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((distance % (1000 * 60)) / 1000);
+  // Display result
+  countdown.innerHTML = `
+  <div>${days}<span>Days</span></div>
+  <div>${hours}<span>Hours</span></div>
+  <div>${mins}<span>Minutes</span></div>
+  <div>${secs}<span>Seconds</span></div>
+  `;
+
+  // If launch date passed 
+  if (distance < 0) {
+    // Stop countdown
+    clearInterval(intvl);
+    // Style and output text
+    countdown.style.color = '#17a2b8';
+  }
+}, 1000);
